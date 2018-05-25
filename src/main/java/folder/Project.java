0ec11043cef19ea81b667143c102128d0fa93cb8
Ivaylo Asenov <iasenov@SEA.CORP.EXPECN.COM>
2018-05-25 15:23:03 -0400

    work

---
+++ src/main/java/folder/Project.java
@@ -0,0 +1,14 @@
+package folder;
+
+public class Project {
+
+    private final String projects;
+
+    public Project(String content){
+        this.projects = content;
+    }
+
+    public String getProjects(){
+        return projects;
+    }
+}
